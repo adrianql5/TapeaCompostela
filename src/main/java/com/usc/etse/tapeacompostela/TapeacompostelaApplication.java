2cd572f92ec76a8b8c7b4f8bcb15e4f277ff08f8
Adrian Quiroga Linares <adrianquiroga5@gmail.com>
2026-09-23 09:39:31 +0200

    Initial Sctructure for the Project, magic begins.

---
+++ src/main/java/com/usc/etse/tapeacompostela/TapeacompostelaApplication.java
@@ -0,0 +1,13 @@
+package com.usc.etse.tapeacompostela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapeacompostelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapeacompostelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usc/etse/tapeacompostela/TapeacompostelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usc.etse.tapeacompostela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapeacompostelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
